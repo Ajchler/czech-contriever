a5bcf4e3d4af29e6fcd7ccb1f22ebd500729b50e
Vojtech Eichler <vojta.eich@gmail.com>
2024-07-03 13:43:39 +0200

    Add script to split data

---
+++ data_scripts/split_data.py
@@ -0,0 +1,22 @@
+import random
+import json
+
+INPUT_FILE = "all-in-one.mosaic.jsonl"
+TRAIN_FILE = "train-portion.jsonl"
+VALID_FILE = "valid-portion.jsonl"
+
+validation_samples_n = 32768
+
+
+with open(INPUT_FILE, "r") as f, open(TRAIN_FILE, "w") as train_f, open(
+    VALID_FILE, "w"
+) as valid_f:
+    valid_indices = set(random.sample(range(0, 177089377), validation_samples_n))
+    for i, line in enumerate(f):
+        line_dict = json.loads(line)
+        line_dict = {"text": line_dict["text"]}
+        line = json.dumps(line_dict, ensure_ascii=False) + "\n"
+        if i in valid_indices:
+            valid_f.write(line)
+        else:
+            train_f.write(line)
