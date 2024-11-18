9e830d5e33c6d5d055ce823c0830f19ee0a44e88
Vojtech Eichler <vojta.eich@gmail.com>
2024-11-18 23:42:31 +0100

    Add more througput evaluation code

--- eval_throughput.py
+++ eval_throughput.py
@@ -1,16 +1,29 @@
 import torch
 import time
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, MarianMTModel
 
 import src.simcse
 import src.contriever
 
 
+def translate_en_cs(en_sentences, max_seq_len, translate_model, tokenizer):
+
+    #inputs = tokenizer(en_sentences, return_tensors="pt", add_special_tokens=False, max_length=max_seq_len, truncation=True)
+    #inputs = {k: v.to("cuda") for k, v in inputs.items()}
+    inputs = torch.randint(0, tokenizer.vocab_size, (len(en_sentences), max_seq_len), dtype=torch.long).to("cuda")
+    translated_sentences = translate_model.generate(inputs, num_beams=4, early_stopping=True)
+
+
 def evaluate_throughput(
-    model, max_seq_length=128, initial_batch_size=1, device="cuda", vocab_size=30522
+    model, max_seq_length=128, initial_batch_size=1, device="cuda", vocab_size=30522, tokenizer=None, translate=False
 ):
     model.to(device)
     model.eval()
+
+    translate_model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-en-cs", cache_dir=".Transformers_cache")
+    translate_model.to("cuda")
+
+    translate_tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-cs", cache_dir=".Transformers_cache")
 
     batch_size = initial_batch_size
     successful_batch_size = batch_size
@@ -39,6 +52,7 @@ def evaluate_throughput(
             else:
                 raise e
 
+    successful_batch_size=32
     # Use maximum batch size to evaluate throughput
     print(f"Max batch size that fits in memory: {successful_batch_size}")
 
@@ -46,6 +60,8 @@ def evaluate_throughput(
     inputs = torch.randint(
         0, vocab_size, (successful_batch_size, max_seq_length), dtype=torch.long
     ).to(device)
+
+    # Decode inputs into actual text
     torch.cuda.synchronize()
     start_time = time.time()
 
@@ -54,6 +70,7 @@ def evaluate_throughput(
 
     with torch.no_grad():
         for _ in range(num_iterations):
+            translated_sentences = translate_en_cs(inputs, max_seq_length, translate_model, translate_tokenizer)
             _ = model(inputs)
 
     torch.cuda.synchronize()
@@ -65,12 +82,12 @@ def evaluate_throughput(
 
 # Example usage:
 # Assume `model` is your embedding model
-model, tokenizer, _ = src.simcse.load_simcse()
-# model = AutoModel.from_pretrained("facebook/contriever")
-# tokenizer = AutoTokenizer.from_pretrained("facebook/contriever")
+#model, tokenizer, _ = src.simcse.load_simcse()
+model = AutoModel.from_pretrained("facebook/contriever")
+tokenizer = AutoTokenizer.from_pretrained("facebook/contriever")
 # model = AutoModel.from_pretrained("facebook/mcontriever")
 # tokenizer = AutoTokenizer.from_pretrained("facebook/mcontriever")
 # model = AutoModel.from_pretrained("Ajchler/czechtriever-demo")
 # tokenizer = AutoTokenizer.from_pretrained("Ajchler/czechtriever-demo")
 
-evaluate_throughput(model, vocab_size=tokenizer.vocab_size)
+evaluate_throughput(model, vocab_size=tokenizer.vocab_size, tokenizer=tokenizer, translate=True)
